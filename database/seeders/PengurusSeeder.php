e2056fda55b4fa31bc898046cf42f98d8b7740ef
reditaa <reditaraesya@gmail.com>
2026-08-10 14:50:01 +0700

    Perbaiki PengurusSeeder sesuai struktur baru

--- database/seeders/PengurusSeeder.php
+++ database/seeders/PengurusSeeder.php
@@ -2,31 +2,65 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\Anggota;
+use App\Models\Jabatan;
 use App\Models\Pengurus;
+use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 
 class PengurusSeeder extends Seeder
 {
     public function run(): void
     {
-        $anggotas = Anggota::where('jenis', 'Guru')->get();
+        $dataPengurus = [
+            [
+                'jabatan' => 'Ketua',
+                'nama' => 'Ketua DKM',
+                'jenis_kelamin' => 'L',
+            ],
+            [
+                'jabatan' => 'Wakil Ketua',
+                'nama' => 'Wakil Ketua DKM',
+                'jenis_kelamin' => 'L',
+            ],
+            [
+                'jabatan' => 'Sekretaris',
+                'nama' => 'Sekretaris DKM',
+                'jenis_kelamin' => 'L',
+            ],
+            [
+                'jabatan' => 'Bendahara',
+                'nama' => 'Bendahara DKM',
+                'jenis_kelamin' => 'P',
+            ],
+            [
+                'jabatan' => 'Humas',
+                'nama' => 'Humas DKM',
+                'jenis_kelamin' => 'L',
+            ],
+        ];
 
-        foreach ($anggotas as $anggota) {
+        foreach ($dataPengurus as $data) {
+            $jabatan = Jabatan::where(
+                'nama_jabatan',
+                $data['jabatan']
+            )->first();
+
+            if (!$jabatan) {
+                continue;
+            }
 
             Pengurus::firstOrCreate(
                 [
-                    'anggota_id' => $anggota->id,
+                    'nama' => $data['nama'],
                 ],
                 [
-                    'jabatan' => 'Imam',
-                    'mulai_jabatan' => Carbon::today(),
-                    'selesai_jabatan' => null,
-                    'status' => 'Aktif',
+                    'jabatan_id' => $jabatan->id,
+                    'jenis_kelamin' => $data['jenis_kelamin'],
+                    'status' => 'aktif',
+                    'periode_mulai' => Carbon::today(),
+                    'periode_selesai' => null,
                 ]
             );
-
         }
     }
 }
